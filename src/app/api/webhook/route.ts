c05cc7c911a230bf1fbcd725b13164a7eac3f830
Aman Pachouri <your-email-addressampachouri09@gmail.com>
2024-05-15 11:48:26 +0530

    fix error handling

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -6,10 +6,17 @@ import sendEmailNotification from "@/utils/emailService";
 
 const WEBHOOK_SECRET = process.env.TWAK_SECRET_KEY;
 
+if (!WEBHOOK_SECRET) {
+  throw new Error(
+    "Webhook secret is not defined in the environment variables."
+  );
+}
+
 async function verifySignature(
   body: string,
   signature: string | null
 ): Promise<boolean> {
+  if (!signature) return false;
   const digest = createHmac("sha1", WEBHOOK_SECRET as string)
     .update(body)
     .digest("hex");
@@ -17,24 +24,30 @@ async function verifySignature(
 }
 
 export async function POST(request: Request) {
-  if (request.method === "POST") {
+  try {
+    if (request.method !== "POST") {
+      return new Response("Method Not Allowed", { status: 405 });
+    }
+
     const body = await request.text();
     const headersList = headers();
     const signature = headersList.get("x-tawk-signature");
 
-    if (!verifySignature(body, signature)) {
+    if (!(await verifySignature(body, signature))) {
       return new Response("Webhook signature verification failed!", {
         status: 403,
       });
     }
+
     await sendEmailNotification(body);
 
     return new Response("Success!", {
       status: 200,
     });
+  } catch (error) {
+    console.error("Error handling webhook request:", error);
+    return new Response("Internal Server Error", {
+      status: 500,
+    });
   }
-
-  return new Response(`Webhook error`, {
-    status: 405,
-  });
 }
